7645a16015ddcd161a4dbf42f975f86d03228b72
Arnaud Georgin <arnaud.georgin.ext@francetv.fr>
2015-06-23 17:21:02 +0200

    Ping event are managed

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 var CheckEvent = require('../../models/checkEvent');
+var Ping = require('../../models/ping');
 var HipchatRoom = require('./component/hipchat/room');
 var config = require('config');
 
 exports.initWebApp = function() {
     var room = new HipchatRoom(config.hipchat.roomId, config.hipchat.token);
+    registerNewEventsHipchat(room);
+    registerNewPingsHipchat(room);
+    console.log('Enabled Hipchat Notification');
+};
+
+var registerNewEventsHipchat = function(room) {
     CheckEvent.on('afterInsert', function(checkEvent) {
-        if (!config.event[checkEvent.message]) {
-            return;
-        }
         checkEvent.findCheck(function(err, check) {
-            if (err) {
-                return console.error(err);
+            var message = check.name + ' ';
+            switch (checkEvent.message) {
+                case 'paused':
+                case 'restarted':
+                    message += 'was ' + checkEvent.message;
+                    break;
+                case 'down':
+                    message += 'went down ' + checkEvent.details;
+                    break;
+                case 'up':
+                    if (checkEvent.downtime) {
+                        message += 'went back up after ' +  Math.floor(checkEvent.downtime / 1000) + 's of downtime';
+                    } else {
+                        message += 'is now up';
+                    }
+                    break;
+                default:
+                    message += '(unknown event)';
             }
 
-
-            room.sendNotification("[Uptime] The application " + check.name  + " just went to status " + checkEvent.message);
-            console.log("[Uptime] The application " + check.name  + " just went to status " + checkEvent.message);
+            room.sendNotification('[Uptime] ' + message);
         });
     });
+};
 
-    console.log('Enabled Hipchat Notification');
+var registerNewPingsHipchat = function(room) {
+    Ping.on('afterInsert', function(ping) {
+        ping.findCheck(function(err, check) {
+            if (!ping.isUp) {
+                room.sendNotification('[Uptime] The application ' + check.name  + ' responded with error "' + ping.error + '"');
+            }
+        });
+    });
 };
